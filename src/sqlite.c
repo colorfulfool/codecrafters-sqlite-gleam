6f973dced099cea771217ffabb213586c80e7c6f
Anton Khamets <anton.khamets@hey.com>
2026-05-26 20:53:52 +0500

    fix endianness

--- src/sqlite.c
+++ src/sqlite.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <arpa/inet.h>
 
 typedef struct {
   char format[16];
-  uint16_t page_size; // 0x1000 => 
+  uint16_t page_size; // hex 10 00 => bin 00010000 00000000
   char empty2[10];
   uint32_t db_size;
 } preamble_t;
@@ -15,11 +16,10 @@ int main(int argc, char** argv) {
   if (argc == 3 && strcmp(argv[2], ".dbinfo") == 0) {
     FILE *file = fopen(argv[1], "r");
 
-    // let assert Ok(_bytes) = read_stream.read_bytes_exact(rs, 16)
-    // let assert Ok(page_size) = read_stream.read_int16_be(rs)
-    // let assert Ok(_bytes) = read_stream.read_bytes_exact(rs, 10)
-    // let assert Ok(db_size) = read_stream.read_int32_be(rs)
-    fread(&preamble, sizeof(preamble_t), 1, file);
+    fread(&preamble, 1, sizeof(preamble_t), file);
+
+    preamble.page_size = ntohs(preamble.page_size);
+    preamble.db_size = ntohl(preamble.db_size);
 
     printf("format: %s\n", preamble.format);
     printf("database page size: %hu\n", preamble.page_size);
